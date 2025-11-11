64bed19f078528517558050b59007a3748c6f7e7
Smak0ff <smakff@gmail.com>
2025-11-11 19:13:12 +0300

    Задание 2. "RUN!" Добавлен класс Run, урл и сериализатор к нему, а также вью. Обновлён гитигнор.

--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from .serializers import RunSerializer
+from .models import Run
 
 
 @api_view(['GET'])
@@ -11,3 +14,8 @@ def company_details_view(request):
         'slogan': settings.SLOGAN,
         'contacts': settings.CONTACTS,
     })
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
--- project_run/urls.py
+++ project_run/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from app_run import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('api/runs', views.RunViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/company_details/', views.company_details_view)
 ]
