608489817ac010c87b2462dfe3a63064b99d364d
Smak0ff <smakff@gmail.com>
2025-11-17 14:21:48 +0300

    обновил инит.

--- app_run/models.py
+++ app_run/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Run(models.Model):
     class Status(models.TextChoices):
         INIT = 'init', 'init'
-        IN_PROGRESS = 'IN_PROGRESS', 'in_progress'
-        FINISHED = 'FINISHED', 'finished'
+        IN_PROGRESS = 'in_progress', 'in_progress'
+        FINISHED = 'finished', 'finished'
     created_at = models.DateTimeField(auto_now_add=True)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
